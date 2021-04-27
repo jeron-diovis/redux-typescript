87e853a59f787febd1aa45a4739bc47575f93cf8
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2021-04-27 14:56:33 +0300

    [setup] add bundle-analyzer plugin

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,13 @@
-const { useBabelRc, override } = require('customize-cra')
+const { useBabelRc, override, addBundleVisualizer } = require('customize-cra')
 
-module.exports = override(useBabelRc())
+module.exports = override(
+  useBabelRc(),
+
+  // @link https://www.npmjs.com/package/webpack-bundle-analyzer
+  addBundleVisualizer({
+    openAnalyzer: false,
+    analyzerPort: 9999, // default 8888 is going to be occupied by backend services
+    analyzerMode: process.env.NODE_ENV === 'production' ? 'static' : 'server',
+    generateStatsFile: true,
+  })
+)
