0cd2632b94bd9b26d237375cffe8ce1aa815d8bd
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2021-05-20 12:31:43 +0300

    [setup] ignore `dist` folder, used by webpack as default outputPath. lint-staged only src files.

--- config-overrides.js
+++ config-overrides.js
@@ -7,7 +7,6 @@ module.exports = override(
   // @link https://www.npmjs.com/package/webpack-bundle-analyzer
   addBundleVisualizer({
     openAnalyzer: false,
-    analyzerPort: 9999, // default 8888 is going to be occupied by backend services
     analyzerMode: process.env.NODE_ENV === 'production' ? 'static' : 'server',
     generateStatsFile: true,
   }),
