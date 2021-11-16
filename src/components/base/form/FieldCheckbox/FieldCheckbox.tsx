59aea143ef0fccdf9a1932dc7e4b7e32b6853c3a
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2021-11-16 19:35:36 +0200

    [setup] FieldCheckbox: cleanup

--- src/components/base/form/FieldCheckbox/FieldCheckbox.tsx
+++ src/components/base/form/FieldCheckbox/FieldCheckbox.tsx
@@ -1,6 +1,6 @@
 import { FieldPath, FieldValues } from 'react-hook-form'
 
-import { Checkbox, ControlSettings } from 'src/components/base/controls'
+import { Checkbox } from 'src/components/base/controls'
 
 import FieldControl from '../FieldControl'
 
@@ -12,32 +12,37 @@ export default function FieldCheckbox<
 >(props: IFieldCheckboxProps<Fields, Name>) {
   const { control = {}, name, rules, ...rest } = props
 
-  const { labelPosition = 'after', stretch = false } = control
+  // Explicit props will take precedence over ControlSettingsContext.
+  // Thus, Checkbox will always render with these settings regardless of context,
+  // unless other props will be passed to it directly.
+  const {
+    labelPosition = 'after',
+    stretch = false,
+    layout = 'max-content auto',
+  } = control
 
   return (
-    /* Force override `columns`. Checkbox is always rendered in a single row */
-    <ControlSettings layout="max-content auto">
-      <FieldControl<Fields, Name>
-        {...control}
-        labelPosition={labelPosition}
-        stretch={stretch}
-        name={name}
-        rules={rules}
-      >
-        {controller => {
-          const { field, fieldState } = controller
-          const { invalid } = fieldState
-          return (
-            <Checkbox
-              {...rest}
-              {...field}
-              checked={field.value}
-              invalid={invalid}
-              onChange={(_, e) => field.onChange(e)}
-            />
-          )
-        }}
-      </FieldControl>
-    </ControlSettings>
+    <FieldControl<Fields, Name>
+      {...control}
+      layout={layout}
+      labelPosition={labelPosition}
+      stretch={stretch}
+      name={name}
+      rules={rules}
+    >
+      {controller => {
+        const { field, fieldState } = controller
+        const { invalid } = fieldState
+        return (
+          <Checkbox
+            {...rest}
+            {...field}
+            checked={field.value}
+            invalid={invalid}
+            onChange={(_, e) => field.onChange(e)}
+          />
+        )
+      }}
+    </FieldControl>
   )
 }
