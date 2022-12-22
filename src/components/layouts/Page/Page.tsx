e1ebddbfcde8cb48f22c4e5f0c8a4f4f783894f1
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2022-12-22 14:11:35 +0200

    [setup] non-scrollable page

--- src/components/layouts/Page/Page.tsx
+++ src/components/layouts/Page/Page.tsx
@@ -5,14 +5,22 @@ import { IPageProps } from './types'
 import styles from './styles.module.scss'
 
 export function Page(props: IPageProps) {
-  const { title, children, center = false } = props
+  const {
+    title,
+    children,
+    center = false,
+    scrollable = true,
+    width = '100%',
+  } = props
   return (
     <div
       className={clsx(styles.page, {
         [styles.page_center]: center === true,
         [styles.page_center_v]: center === 'v',
         [styles.page_center_h]: center === 'h',
+        [styles.page_non_scrollable]: !scrollable,
       })}
+      style={width === undefined ? undefined : { width }}
     >
       <If condition={title !== undefined}>
         <h2 className={styles.page_title}>{title}</h2>
--- src/components/layouts/Page/types.ts
+++ src/components/layouts/Page/types.ts
@@ -1,7 +1,9 @@
-import { ReactNode } from 'react'
+import { CSSProperties, ReactNode } from 'react'
 
 export interface IPageProps {
   title?: ReactNode
   center?: boolean | 'v' | 'h'
+  scrollable?: boolean
+  width?: CSSProperties['width']
   children: ReactNode
 }
