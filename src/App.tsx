a831a31f4ff7bb2c4bb55940d7794d789c4578fc
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2023-02-08 11:20:55 +0200

    Apply lint to sources

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
-import { useState } from 'react'
+import clsx from 'clsx'
+
+import Counter from 'src/Counter'
+
 import reactLogo from './assets/react.svg'
-import './App.css'
+
+import styles from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
+    <div className={styles.app}>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className={styles.logo} alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img
+            src={reactLogo}
+            className={clsx(styles.logo, styles.react)}
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className={styles.card}>
+        <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className={styles.read_the_docs}>
         Click on the Vite and React logos to learn more
       </p>
     </div>
