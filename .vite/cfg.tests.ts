7e12f76d67052fe1770176a3b7a99774d9311f89
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2023-09-04 23:29:33 +0300

    [setup] fix wtf with vitest typings

--- .vite/cfg.tests.ts
+++ .vite/cfg.tests.ts
@@ -1,8 +1,11 @@
-/// <reference types="vitest" />
 import { defineChunk } from 'vite-split-config'
 import { configDefaults } from 'vitest/config'
 
-export const useTests = defineChunk({
+/**
+ * @see https://github.com/vitest-dev/vitest/discussions/1106#discussioncomment-2531279
+ * 04.09.23: MUST use function form. Probably return type widening helps.
+ */
+export const useTests = defineChunk(() => ({
   test: {
     globals: true,
     environment: 'jsdom',
@@ -13,10 +16,10 @@ export const useTests = defineChunk({
      * Uncomment to enable in-source testing.
      * @see https://vitest.dev/guide/in-source.html
      */
-    // includeSource: ['src/**/*.{js,ts}{,x}'],
+    // includeSource: ['src/!**/!*.{js,ts}{,x}'],
   },
   /**
    * @see https://vitest.dev/guide/in-source.html#production-build
    */
   define: { 'import.meta.vitest': 'undefined' },
-})
+}))
