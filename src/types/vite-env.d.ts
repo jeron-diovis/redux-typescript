7f5c20e1d2b4d5d4fd93d0c94c5ba19d9d6eb8b9
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2023-11-06 11:25:29 +0200

    [setup] global types for .svg modules

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -1,7 +1,2 @@
 /// <reference types="vite/client" />
-
-declare module '*.svg' {
-  import { Component, SVGAttributes } from 'react'
-
-  export const ReactComponent = Component<SVGAttributes<SVGElement>>
-}
+/// <reference types="vite-plugin-svgr/client" />
