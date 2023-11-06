7371ed9bd5127a35543bf5748d2ea841a3b0b4db
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2023-11-06 11:03:02 +0200

    [setup] update node-compat related plugins

--- .vite/cfg.node-compat.ts
+++ .vite/cfg.node-compat.ts
@@ -1,25 +1,28 @@
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 import { Plugin } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { defineChunk } from 'vite-split-config'
 
 /**
  * @see https://stackoverflow.com/a/70666018/3437433
  */
 export const useNodeCompat = defineChunk({
+  plugins: [
+    nodePolyfills({
+      include: ['events'],
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        process: true,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   optimizeDeps: {
     esbuildOptions: {
       define: {
         global: 'globalThis', // Node.js global to browser globalThis
       },
-      plugins: [
-        NodeModulesPolyfillPlugin(),
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-      ],
     },
   },
 
