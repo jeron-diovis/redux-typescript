6b0dee8a6b8d2b2bf8122245e969b8a890a923ec
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2023-11-09 13:55:05 +0200

    [setup] fix type declarations for jsx-control-statements

--- src/types/jsx-if.d.ts
+++ src/types/jsx-if.d.ts
@@ -2,10 +2,10 @@ import { FC, PropsWithChildren } from 'react'
 
 declare global {
   // @see https://github.com/AlexGilleran/jsx-control-statements/issues/72#issuecomment-389484553
-  declare const If: FC<PropsWithChildren<{ condition: boolean }>>
-  declare const Choose: FC<PropsWithChildren>
-  declare const When: FC<PropsWithChildren<{ condition: boolean }>>
-  declare const Otherwise: FC<PropsWithChildren>
+  const If: FC<PropsWithChildren<{ condition: boolean }>>
+  const Choose: FC<PropsWithChildren>
+  const When: FC<PropsWithChildren<{ condition: boolean }>>
+  const Otherwise: FC<PropsWithChildren>
   // Don't support <With> / <For>, because they are hard to type properly,
   // and only complicate code readability anyway.
 }
