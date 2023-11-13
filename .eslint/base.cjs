110e5066e83d85cf170b61a33f8f148e2462bbdc
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2023-11-13 16:58:59 +0200

    [setup] move base eslint rules into seprate module

---
+++ .eslint/base.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-unreachable': 'warn',
+    'no-var': 'error',
+    'prefer-const': 'warn',
+    'no-param-reassign': [
+      'error',
+      {
+        // Allow to modify props. Mostly, to support immer producers.
+        props: false,
+      },
+    ],
+    'no-duplicate-imports': 'warn',
+    'object-shorthand': 'warn',
+    'one-var': ['error', 'never'],
+    'no-underscore-dangle': [
+      'error',
+      {
+        allowAfterThis: true,
+      },
+    ],
+    'prefer-arrow-callback': [
+      'warn',
+      {
+        allowUnboundThis: false,
+        allowNamedFunctions: true,
+      },
+    ],
+
+    'no-nested-ternary': 'warn',
+    'no-unneeded-ternary': 'warn',
+
+    'react/jsx-key': 'warn',
+    'react/jsx-boolean-value': 'warn',
+  },
+}
--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,47 +3,12 @@ module.exports = {
     'react-app',
     'eslint:recommended',
     'plugin:storybook/recommended',
+    './.eslint/base.cjs',
     './.eslint/typescript.cjs',
     './.eslint/jsx-control-statements.cjs',
     './.eslint/imports.cjs',
     './.eslint/prettier.cjs',
   ],
-
-  rules: {
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'no-unreachable': 'warn',
-    'no-var': 'error',
-    'prefer-const': 'warn',
-    'no-param-reassign': [
-      'error',
-      {
-        // Allow to modify props. Mostly, to support immer producers.
-        props: false,
-      },
-    ],
-    'no-duplicate-imports': 'warn',
-    'object-shorthand': 'warn',
-    'one-var': ['error', 'never'],
-    'no-underscore-dangle': [
-      'error',
-      {
-        allowAfterThis: true,
-      },
-    ],
-    'prefer-arrow-callback': [
-      'warn',
-      {
-        allowUnboundThis: false,
-        allowNamedFunctions: true,
-      },
-    ],
-
-    'no-nested-ternary': 'warn',
-    'no-unneeded-ternary': 'warn',
-
-    'react/jsx-key': 'warn',
-    'react/jsx-boolean-value': 'warn',
-  },
 
   overrides: [
     {
