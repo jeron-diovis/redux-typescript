e604861c16d81163beb263b60162a72dc56a9e76
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2023-11-13 16:20:20 +0200

    [setup] some clean up `lintstagedrc`

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -4,6 +4,12 @@ const eslint = new ESLint()
 
 /**
  * @see https://stackoverflow.com/questions/37927772/how-to-silence-warnings-about-ignored-files-in-eslint
+ * @see https://github.com/lint-staged/lint-staged#eslint--7-1
+ * With eslint it's impossible to silently run it over ignored files. They must be explicitly excluded from command's args.
+ *
+ * Warning can be suppressed with `--no-warn-ignored` option
+ * @see https://eslint.org/docs/latest/use/command-line-interface#--no-warn-ignored
+ * but that's only works with a new "flat config" mode, which requires migration.
  */
 async function removeIgnoredFiles(files) {
   const is_ignored = await Promise.all(
@@ -13,14 +19,16 @@ async function removeIgnoredFiles(files) {
   return filtered.join(' ')
 }
 
-const paths = '{src,mock,tests}'
+const paths = '{src,mock,tests}/**'
 
 module.exports = {
-  [`${paths}/**/*.{cjs,js,ts,jsx,tsx}`]: async files => [
-    `eslint --cache --fix --max-warnings=0 ${await removeIgnoredFiles(files)}`,
+  [`${paths}/*.{c,}{j,t}s{x,}`]: async files => [
+    `eslint --cache --fix --max-warnings=0 --no-error-on-unmatched-pattern ${await removeIgnoredFiles(
+      files
+    )}`,
   ],
 
-  [`${paths}/**/*.{s,}css`]: [
+  [`${paths}/*.{s,}css`]: [
     'stylelint --cache --fix --max-warnings=0 --allow-empty-input',
   ],
 }
