d03a7618f772ddf1f76df74287ad51b493d9b2e6
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2024-05-15 15:55:53 +0300

    Remove seemingly unnecessary alias from vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,6 @@ export default defineConfig({
     /** Note these aliases imply css files too – affecting paths in `composes` prop. */
     alias: {
       src: sysPath.resolve(__dirname, 'src'),
-
-      /* Should be used only inside `mock` folder.
-       * To make intellisense work, also requires to define this alias in `mock/tsconfig.json` */
-      '~mock': sysPath.resolve(__dirname, 'mock'),
     },
   },
 })
