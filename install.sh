a3cb5b507ab0d9e2dc585c5d82f844be5cc35353
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2024-05-21 22:16:40 +0300

    update installation script: detect yarn or npm setup

--- install.sh
+++ install.sh
@@ -1,14 +1,38 @@
 #!/bin/sh
 
+# ---
+
+if [ -f yarn.lock ]; then
+  CMD_INSTALL="yarn add"
+  PROP_OVERRIDES="resolutions"
+else
+  CMD_INSTALL="npm install --audit=false --fund=false"
+  PROP_OVERRIDES="overrides"
+fi
+
+function install() {
+  $CMD_INSTALL "$@"
+}
+
+function override() {
+  npm pkg set "$PROP_OVERRIDES.$1=$2"
+}
+
+function npm_script() {
+  npm pkg set "scripts.$1=$2"
+}
+
+# ---
+
 function add_eslint() {
   echo Install eslint
 
   # have to pin it to make eslint@8 and vite-plugin-checker a friends
-  npm pkg set resolutions.meow="^9.0.0"
+  override meow "^9.0.0"
   echo .eslintcache >> .gitignore
 
   # Pin eslint to v8, because v9 seems to be incompatible with vite-plugin-checker@0.6.4
-  yarn add -D eslint@^8.0.0 \
+  install -D eslint@^8.0.0 \
     @typescript-eslint/eslint-plugin \
     @typescript-eslint/parser \
     eslint-plugin-react-hooks \
@@ -23,7 +47,7 @@ function add_eslint() {
 function add_styles() {
   echo Install stylelint and CSS preprocessor
   echo .stylelintcache >> .gitignore
-  yarn add -D sass \
+  install -D sass \
     stylelint \
     stylelint-config-css-modules \
     stylelint-config-standard-scss \
@@ -35,14 +59,14 @@ function add_styles() {
 
 function add_jsx_if() {
   echo Install jsx-control-statements
-  yarn add -D eslint-plugin-jsx-control-statements \
+  install -D eslint-plugin-jsx-control-statements \
     babel-plugin-jsx-control-statements \
     @babel/plugin-transform-react-jsx
 }
 
 function add_vite_plugins() {
   echo Install Vite quality-of-life plugins
-  yarn add -D @vitejs/plugin-react \
+  install -D @vitejs/plugin-react \
     vite-split-config \
     vite-plugin-checker \
     vite-plugin-importus \
@@ -59,10 +83,10 @@ function add_vite_plugins() {
   # Add types for SVGR plugin
   TYPES='/// <reference types="vite-plugin-svgr/client" />'
   FILES=(vite-env.d.ts, types/vite-env.d.ts)
-  for item in ${FILES[@]}; do
-    PATH=src/$item
-    if [ -f "$PATH" ]; then
-      echo $TYPES >> $PATH
+  for item in "${FILES[@]}"; do
+    FILEPATH=src/$item
+    if [ -f "$FILEPATH" ]; then
+      echo "$TYPES" >> "$FILEPATH"
       break
     fi
   done
@@ -71,20 +95,20 @@ function add_vite_plugins() {
 
 function add_musthave_packages() {
   echo Install must-have utility packages
-  yarn add lodash-es date-fns \
+  install lodash-es date-fns \
     clsx axios query-string utility-types
-  yarn add -D @types/lodash-es @types/node
+  install -D @types/lodash-es @types/node
 }
 
 function add_precommit() {
   echo Install git-hooks tools
-  yarn add -D 'husky@^7.0.0' lint-staged
+  install -D 'husky@^7.0.0' lint-staged
   ./init-git-hooks.sh
 }
 
 function add_tests() {
   echo Install testing utilities
-  yarn add -D vitest \
+  install -D vitest \
     @vitest/ui \
     @testing-library/react \
     @testing-library/jest-dom
@@ -92,28 +116,35 @@ function add_tests() {
 
 function add_npm_scripts() {
   echo Add npm scripts to package.json
-  npm pkg set 'scripts.stat'='./view-stats.sh'
-  npm pkg set "scripts.lint:js"="eslint src --ext .cjs,.js,.jsx,.ts,.tsx"
-  npm pkg set "scripts.lint:ts"="tsc --noEmit && cd mock && tsc --noEmit"
-  npm pkg set "scripts.lint:css"="stylelint \"src/**/*.{s,}css\""
-  npm pkg set "scripts.lint"="yarn lint:js && yarn lint:ts && yarn lint:css"
-  npm pkg set "scripts.fix"="yarn lint:js --fix && yarn lint:css --fix"
-  npm pkg set "scripts.test"="vitest"
+  npm_script stat './view-stats.sh'
+  npm_script lint:js "eslint src --ext .cjs,.js,.jsx,.ts,.tsx"
+  npm_script lint:ts "tsc --noEmit && cd mock && tsc --noEmit"
+  npm_script lint:css "stylelint \"src/**/*.{s,}css\""
+  npm_script lint "yarn lint:js && yarn lint:ts && yarn lint:css"
+  npm_script fix "yarn lint:js --fix && yarn lint:css --fix"
+  npm_script test "vitest"
 }
 
 function edit_ts_config() {
   echo Adjust tsconfig with types and plugins
-  yarn add -D dot-json
+  install -D dot-json
+
+  function remove_comments() {
+    sed -i '' -e '/^[[:space:]]*\/\*.*\*\//d' "$1"
+  }
+
+  function json_prop() {
+    dot-json "$1" -j "$2" "$3"
+  }
 
   # remove block comments (which may be added by Vite's scaffolder, for example)
   # Because any json-parsers will break at comment lines
-  RE='/^[[:space:]]*\/\*.*\*\//d'
-  sed -i '' -e $RE tsconfig.json
-  sed -i '' -e $RE tsconfig.node.json
+  remove_comments tsconfig.json
+  remove_comments tsconfig.node.json
 
-  dot-json tsconfig.json -j compilerOptions.plugins '[{ "name": "typescript-plugin-css-modules" }]'
-  dot-json tsconfig.json -j compilerOptions.types '["vitest", "vitest/globals", "vitest/importMeta", "@testing-library/jest-dom"]'
-  dot-json tsconfig.node.json -j include '["vite.config.ts", ".vite/**/*.ts", "package.json"]'
+  json_prop tsconfig.json compilerOptions.plugins '[{ "name": "typescript-plugin-css-modules" }]'
+  json_prop tsconfig.json compilerOptions.types '["vitest", "vitest/globals", "vitest/importMeta", "@testing-library/jest-dom"]'
+  json_prop tsconfig.node.json include '["vite.config.ts", ".vite/**/*.ts", "package.json"]'
 }
 
 function main() {
