259a23ebd982a3c907b8799b865210d4fba20196
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2024-05-27 15:55:50 +0300

    Make mock-dev-server plugin work again, using vite aliases instead of tsconfig-paths.

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,19 @@ export default defineConfig({
     /** Note these aliases imply css files too – affecting paths in `composes` prop. */
     alias: {
       src: sysPath.resolve(__dirname, 'src'),
+
+      /* Handy alias to navigate a vast nested mocks structure.
+       * Supposed to be used _only_ inside '/mock' folder.
+       * DO NOT import it in app sources. */
+      '~mock': sysPath.resolve(__dirname, 'mock'),
     },
   },
 
-  plugins: [tsconfigPaths()],
+  plugins: [
+    /* Note that this plugin is NOT equivalent to `resolve.alias` option.
+     * It does resolve imports – yet if, for example, some plugin relies
+     * specifically on an alias configured (like "mock-dev-server" plugin does) –
+     * tsconfig-paths will NOT help with that. */
+    tsconfigPaths(),
+  ],
 })
