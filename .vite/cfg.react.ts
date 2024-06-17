97260adf4b8075149ca32961622f6a6fa9418347
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2024-06-17 15:36:38 +0300

    Add vite-plugin-react-click-to-component

--- .vite/cfg.react.ts
+++ .vite/cfg.react.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react'
+import { reactClickToComponent } from 'vite-plugin-react-click-to-component'
 import reactControlStatements from 'vite-plugin-react-control-statements'
 import svgr from 'vite-plugin-svgr'
 import { defineChunk } from 'vite-split-config'
@@ -8,5 +9,6 @@ export const useReact = defineChunk({
     react(),
     reactControlStatements(),
     svgr(), // import { ReactComponent } from '*.svg?react'
+    reactClickToComponent() /* @see https://github.com/ArnaudBarre/vite-plugin-react-click-to-component */,
   ],
 })
