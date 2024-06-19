53573dda27fc09dbe9ea5b39832099fc1dc76481
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2024-06-19 19:34:38 +0300

    Rewrite eslint configs to yml

--- .eslint/prettier.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['plugin:prettier/recommended'],
-
-  rules: {
-    // prettier handles this on it's own
-    quotes: 0,
-    '@typescript-eslint/no-extra-semi': 0,
-  },
-}
